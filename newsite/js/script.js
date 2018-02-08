a9f397f629ff837e439356187a901a5a6db81962
Crunchbot <systems+crunchbot@crunch.io>
2018-02-08 13:17:30 +0000

    Updating test version of company website (build 147)

---
+++ newsite/js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    $('[data-toggle="popover"]').popover({
+        container: 'body',
+        html: true,
+        placement: 'right',
+        trigger: 'hover',
+        title: function() {
+            return $(this).parent().find('.popover-title').html();
+        },
+        content: function() {
+            return $(this).parent().find('.popover-content').html();
+        }
+    });
+
+    //Footer
+    var $window = $(window),
+    $footer = $('.footer');
+
+    function resize() {
+        if ($window.width() < 576) {
+            return $footer.removeClass('justify-content-center');
+        }
+        else {
+            return $footer.addClass('justify-content-center');
+        }
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
+
+
+    //Homepage img animation
+    $(window).scroll(function(){
+        if($(window).scrollTop() <= 1000){
+          $('.crunch-interface').addClass('animated fadeInUp');
+          $('.crunch-interface').removeClass('hidden');
+        } 
+    });
+
+});
