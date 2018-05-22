b6f9d60cb57a36ae7c5c78f6b1f2b82c85b34407
GShotwell <gordon@crunch.io>
2018-05-22 10:18:05 -0300

    Updated mocks, removed shinytest remotes

--- tests/testthat/gadgets/makeArrayGadget/app.R
+++ tests/testthat/gadgets/makeArrayGadget/app.R
@@ -1,6 +1,8 @@
 # You may need to install and restart the package for this to work. Load_all
 # will not pick up the most recent changes, and so testing can fail if the installed
 # version of crunch differs from your working version. 
+# In particular if your tests pass locally, but fail on check or Travis, then
+# likely you need to install and restart.
 library(crunchy)
 source(system.file("crunch-test.R", package = "crunch"))
 
