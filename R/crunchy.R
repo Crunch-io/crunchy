ae01c447431e771a11d37110ca46728b13be933b
GShotwell <gordon@crunch.io>
2018-06-07 13:56:11 -0300

    Revert "Try this approach to ensuring authentication"
    This reverts commit 3a58ed10cef514171a5d3e5a75ac467dd203b455.

--- R/crunchy.R
+++ R/crunchy.R
@@ -2,16 +2,5 @@
 #' @importFrom crunch crGET
 .onLoad <- function (lib, pkgname="crunchy") {
     injectCrunchAssets()
-    ## Hack: make sure the request to the API root inside of me() is uncached
-    ## so that we can use this request to ensure that the current user is
-    ## authenticated with Crunch.
-    suppressMessages(trace(
-        "me",
-        tracer=quote({
-            userURL <- function () uncached(crGET(getOption("crunch.api")))$urls$user_url
-        }),
-        where=tokenAuth,
-        print=FALSE
-    ))
     invisible()
 }
--- R/shiny-user.R
+++ R/shiny-user.R
@@ -1,18 +1,21 @@
-#' Get Crunch user details and confirm authentication
+#' Return Crunch User Information
 #'
-#' `shinyUser()` returns information about the current user of the Shiny app. This
+#' This function returns information about the current user of the shiny app. This
 #' is useful if you want to change the behavior of the app depending on who is
-#' viewing the app. You can access elements of this record with Crunch functions
+#' viewing the app. You can access elements of this record with crunch functions
 #' like `name()` or `email().`
-#'
-#' `checkAuthentication()` is an alias for `shinyUser()` for when you only care
-#' about ensuring that the current user is authenticated with Crunch.
-#'
+#' 
+#' @param text_error If TRUE the function will return a character vector instead of
+#' erroring. This is useful for handling the error in your shiny app.  
 #' @return A user record if the user is logged in, otherwise a character vector or error
 #' @export
-#' @importFrom crunch me
-shinyUser <- function () .buildReactiveExpr("me")
+shinyUser <- function () .buildReactiveExpr(".getUserRecord")
 
-#' @rdname shinyUser
 #' @export
-checkAuthentication <- shinyUser
+#' @inheritParams shinyUser
+#' @importFrom httpcache uncached
+#' @importFrom crunch me
+#' @keywords internal
+.getUserRecord <- function(text_error = TRUE) {
+        uncached(me())
+}
