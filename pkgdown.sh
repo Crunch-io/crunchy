7041ccb19fd67a3702a6cdb2995820e4b2d20d3e
Arlind Qirjazi <arlind@crunch.io>
2020-12-23 14:32:34 +0100

    testing-gh-actions

--- pkgdown.sh
+++ pkgdown.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] &&
-    [ "${TRAVIS_TAG}" != "" ] &&
-    [ "${TRAVIS_R_VERSION_STRING}" = "release" ]; then
-
-    R CMD INSTALL . ## Package needs to be installed, apparently
-    R -e 'install.packages("pkgdown"); library(crunchy); pkgdown::build_site()'
+if [ "${RELEASE_VERSION}" != "" ]; then
     git clone --branch src https://${GH_TOKEN}@github.com/Crunch-io/ta-da.git ../ta-da
     rm -rf ../ta-da/static/r/crunchy
     cp -r docs/. ../ta-da/static/r/crunchy
     cd ../ta-da
     git add .
-    git commit -m "Updating crunchy pkgdown site (release ${TRAVIS_TAG})" || true
+    git commit -m "Updating crunchy pkgdown site (release ${RELEASE_VERSION})" || true
     git push origin src || true
 fi
