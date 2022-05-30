c2b3921ef39d5b3d5b89b9f3e783f555e613fe48
Crunchbot <systems+crunchbot@crunch.io>
2022-05-30 10:09:12 +0000

    Updating test version of company website (build 681)

--- newsite/js/script.js
+++ newsite/js/script.js
@@ -169,7 +169,6 @@ $(document).ready(function() {
     $('.video-btn').click(function() {
         $videoSrc = $(this).data( "src" );
     });
-    console.log($videoSrc);
 
     $('#myModal').on('shown.bs.modal', function (e) {
         $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" );
@@ -178,6 +177,27 @@ $(document).ready(function() {
     $('#myModal').on('hide.bs.modal', function (e) {
         $("#video").attr('src',$videoSrc);
     })
+
+
+    // Hidden elements of accordion that ar active
+    var accordionTargets;
+    $("#accordion-container .accordion .card-title button").on("click",function(e){
+<<<<<<< HEAD
+
+      accordionTargets = $(this).data("target");
+      $('#accordion-container .collapse.show').collapse('hide');
+      // we forsure that the carousel to open after 380 milliseconds
+      setTimeout(function(){
+        $(accordionTargets).collapse('show');
+      },380);
+
+=======
+      accordionTargets = $(this).data("target");
+      $(accordionTargets).on('show.bs.collapse', function (e) {
+        jQuery(".hide.collapse.show").collapse('hide');
+      })
+>>>>>>> visual-fixes-and-interaction-on-the-career-page
+    });
 
 // End
 });
